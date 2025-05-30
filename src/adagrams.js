4a34d21fbd5359c574ac14ee2e15d4ca08727841
Natalia Razmadze <natasha.razmadze@gmail.com>
2025-05-29 22:05:33 -0700

    Implemented wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -7,6 +7,16 @@ const LETTER_POOL = {
   Z: 1
 };
 
+const SCORE_CHART = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10
+};
+
 export const drawLetters = () => {
 
   const bag = [];
@@ -16,9 +26,6 @@ export const drawLetters = () => {
       bag.push(letter);
     }
   }
-  
-  console.log("bag = ");
-  console.log(bag);
 
   const hand = [];
   for (let i = 0; i < 10; i++) {
@@ -26,9 +33,6 @@ export const drawLetters = () => {
     hand.push(bag[randIndex]);
     bag.splice(randIndex, 1);
   }
-
-  console.log("hand = ");
-  console.log(hand);
 
   return hand;
 };
@@ -50,9 +54,22 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+
+  const upperWord = word.toUpperCase();
+
+  let totalScore = 0;
+
+  for (const char of upperWord) {
+    totalScore += SCORE_CHART[char] || 0;
+    }
+
+  if (upperWord.length >= 7 && upperWord.length <= 10) {
+    totalScore += 8;
+  }
+
+  return totalScore;
+
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
 };
